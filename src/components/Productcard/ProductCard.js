5154bc98d6cc7e2a5443a70be11242bfdec25738
vineet-coder <vineetsinghal2312@gmail.com>
2022-03-02 13:36:48 +0530

    products filter with redux

--- src/components/Productcard/ProductCard.js
+++ src/components/Productcard/ProductCard.js
@@ -4,13 +4,14 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    padding: "1rem",
+    margin: " 3rem 1rem",
   },
   media: {
     height: 140,
@@ -22,21 +23,15 @@ export default function ProductCard({ product }) {
   return (
     <Card className={classes.root} id="product-card">
       <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image={product.productImage}
-          title="Contemplative Reptile"
-        />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            ₹{product.price}
+            ₹{product.name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {product.productName}{" "}
-            <lable className="category-tag">({product.category})</lable>
+          <Typography variant="h6" color="textSecondary" component="h3">
+            {product.price} Rs
           </Typography>
-          <Typography gutterBottom variant="h6" component="h5">
-            SIZE - {product.size}
+          <Typography gutterBottom variant="caption" component="h5">
+            Description: {product.description}
           </Typography>
         </CardContent>
       </CardActionArea>
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { FilterProvider } from "./providers/filter/FilterContext";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FilterProvider>
-      <App />
-    </FilterProvider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
